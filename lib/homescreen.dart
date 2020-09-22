12e6bc414991ee3c3b90cd30023413ef98b1eb04
Ayush mehta <ayushmehta651@gmail.com>
2020-09-22 23:09:05 +0530

    Added HomeScreen

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int leftDiceNumber = 1;
-  int rightDiceNumber = 2;
+  int rightDiceNumber = 1;
   void roll() {
     setState(() {
       leftDiceNumber = Random().nextInt(6) + 1;
@@ -19,44 +19,49 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dicee'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.all(15),
-                    child: Image(
-                      image: AssetImage(
-                          'assets/images/dice-png-$leftDiceNumber.png'),
+      appBar: AppBar(title: Text('LUDO')),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Image(
+                        image: AssetImage(
+                            'assets/images/dice-png-$leftDiceNumber.png'),
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.all(15),
-                    child: Image(
-                      image: AssetImage(
-                          'assets/images/dice-png-$rightDiceNumber.png'),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Image(
+                        image: AssetImage(
+                            'assets/images/dice-png-$rightDiceNumber.png'),
+                      ),
                     ),
                   ),
+                ],
+              ),
+              RaisedButton(
+                color: Colors.green[400],
+                onPressed: roll,
+                child: Text(
+                  'Roll Dice',
+                  style: (TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontFamily: 'Pacifico',
+                  )),
                 ),
-              ],
-            ),
-            RaisedButton(
-              onPressed: () {
-                roll();
-              },
-              child: Text('Roll'),
-              disabledColor: Colors.green,
-              hoverElevation: 5.0,
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
